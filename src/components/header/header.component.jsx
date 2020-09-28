feab9645f9701c86bada938088aa9061e0faa1e1
kailash9820 <kailashgaribidi9820@gmail.com>
2020-09-28 22:42:17 +0530

    added reux,and implimented userReducer and userAction to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 const Header =({currentUser})=>(
@@ -22,6 +23,9 @@ const Header =({currentUser})=>(
         </div>
     </div>
 )
+const mapStateToProps=state=>({
+currentUser:state.user.currentUser
 
-export default Header;
+});
+export default connect(mapStateToProps)(Header);
 
