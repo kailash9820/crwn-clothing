290ce5ea42a16ce5c9ea32a417a5bbd21ad39b9c
kailash9820 <kailashgaribidi9820@gmail.com>
2020-11-08 21:08:07 +0530

    added checkout-item into checkout page and updated cart to use selector

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch,Route,Redirect} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import CheckoutPages from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import SignAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth,createUserProfileDocument } from './firebase/firebase.utils';
@@ -41,6 +42,7 @@ class App extends React.Component{
        <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route exact path='/checkout' component={CheckoutPages}/>
         <Route exact path='/signin' render={()=>this.props.currentUser?(<Redirect to='/'/>):(<SignAndSignUpPage/>)}/>
       </Switch>
      
